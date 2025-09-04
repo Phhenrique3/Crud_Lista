14cac0a09e5b3474765f9cacf43373cba9059281
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-09-04 19:22:56 -0300

    Crud_list

---
+++ src/test/java/com/example/crud_lista/CrudListaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_lista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudListaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
